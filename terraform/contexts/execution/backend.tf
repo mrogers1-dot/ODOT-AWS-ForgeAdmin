43a5a584ca483a4c8247b46e017b8a50eb5ad724
ftvizsla <154461918+ftvizsla@users.noreply.github.com>
2026-06-09 15:15:04 -0400

    ops: add S3 backend configuration to all terraform modules

---
+++ terraform/contexts/execution/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "forgeadmin-terraform-state"
+    key            = "forgeadmin/execution/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "forgeadmin-terraform-locks"
+    encrypt        = true
+  }
+}
